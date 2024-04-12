3fafe692aa3805d003800bbf9373a3a0177e6363
jordybsk <dylan.bossoku@jobtrek.ch>
2024-04-12 14:15:48 +0200

    feat: sorted all data by ip

--- Interface/src/elements/plan/PlanElement.tsx
+++ Interface/src/elements/plan/PlanElement.tsx
@@ -1,28 +1,41 @@
 import CardPlanElement from "@/elements/plan/CardPlanElement.tsx";
 import SideBarElement from "@/elements/SideBarElement.tsx";
-import { useEffect, useState } from "react";
-import { SampleContext } from "@/contexts/SampleContext.tsx";
-import { Esp, getToken } from "@/contexts/lib.tsx";
+import {useEffect, useState} from "react";
+import {SampleContext} from "@/contexts/SampleContext.tsx";
+import {Data, Esp, getToken} from "@/contexts/lib.tsx";
+import AddRoomElement from "@/elements/plan/AddRoomElement.tsx";
 
 export default function PlanElement() {
     const apiAuthToken = getToken();
-
-    const [roomName, setRoomName] = useState('');
-
-    const [newIp, setNewIp] = useState('');
-
     const [espData, setEspData] = useState<Esp[]>([]);
+    const [Data, setData] = useState<Data[]>([]);
+
 
     useEffect(() => {
-        fetch(`${SampleContext.urlData}/esp`, { "headers": { "Authorization": `Bearer ${apiAuthToken}` } })
+        fetch(`${SampleContext.urlData}/esp`, {"headers": {"Authorization": `Bearer ${apiAuthToken}`}})
             .then(response => response.json())
-            .then((apiData: Esp[]) => {
-                setEspData(apiData);
+            .then((dataEsp: Esp[]) => {
+                setEspData(dataEsp);
             })
             .catch(error => {
                 console.error('Une erreur s\'est produite:', error);
             });
     }, []);
+
+    useEffect(() => {
+        fetch(`${SampleContext.urlData}/data`, {"headers": {"Authorization": `Bearer ${apiAuthToken}`}})
+            .then(response => response.json())
+            .then((apiData: Data[]) => {
+                const groupedData: { [key: string]: Data[] } = {};
+                espData.forEach((esp) => {
+                    groupedData[esp.ip] = apiData.filter((data) => data.ip === esp.ip);
+                });
+                console.log("Données groupées par adresse IP :", groupedData);
+            })
+            .catch(error => {
+                console.error('Une erreur s\'est produite:', error);
+            });
+    });
 
     const groupedData = espData.reduce((acc: { [key: string]: Esp[] }, currentValue) => {
         if (!acc[currentValue.name]) {
@@ -31,29 +44,6 @@ export default function PlanElement() {
         acc[currentValue.name].push(currentValue);
         return acc;
     }, {});
-
-    const addRoom = () => {
-        fetch(`${SampleContext.urlData}/esp`, {
-            method: 'POST',
-            headers: {
-                'Authorization': `Bearer ${apiAuthToken}`,
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                ip: newIp,
-                name: roomName
-            })
-        })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Erreur lors de la requête');
-                }
-                console.log('Requête POST réussie');
-            })
-            .catch(error => {
-                console.error('Une erreur s\'est produite:', error);
-            });
-    };
 
     return (
         <>
@@ -70,33 +60,8 @@ export default function PlanElement() {
                         />
                     ))}
                 </div>
-                <form className="flex gap-8 h-8" onSubmit={(e) => {
-                    e.preventDefault();
-                    addRoom();
-                }}>
-                    <input
-                        value={roomName}
-                        autoComplete={"roomName"}
-                        onChange={(e) => setRoomName(e.target.value)}
-                        className="mt-5 tracking-wide font-semibold text-white-500 w-full px-4 py-4 rounded-lg transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
-                        type="text" placeholder="Room name"/>
-
-                    <input
-                        value={newIp}
-                        autoComplete={"newIp"}
-                        onChange={(e) => setNewIp(e.target.value)}
-                        className="mt-5 tracking-wide font-semibold text-white-500 w-full px-4 py-4 rounded-lg transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
-                        type="text" placeholder="New IP"/>
-
-                    <button
-                        type="submit"
-                        className="mt-5 tracking-wide font-semibold bg-blue-400 text-white-500 w-full py-4 rounded-lg hover:bg-blue-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
-                        Add a room
-                    </button>
-                </form>
+                <AddRoomElement/>
             </div>
-
-
         </>
     )
 }
