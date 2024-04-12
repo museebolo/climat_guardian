0474c16148922d8b1d65fdf61e6bfa7602215f75
jordybsk <dylan.bossoku@jobtrek.ch>
2024-04-12 09:00:33 +0200

    feat: add new plan card

--- Interface/src/contexts/SampleContext.example.tsx
+++
@@ -1,4 +0,0 @@
-export const SampleContext = {
-    urlData: "",
-    urlLogin: ""
-}
---
+++ Interface/src/elements/plan/CardPlanElement.tsx
@@ -0,0 +1,29 @@
+import {Card, CardHeader, CardTitle} from "@/components/ui/card.tsx";
+
+export default function CardPlanElement() {
+
+
+    return (
+        <>
+            <Card className="bg-white p-4 dark:bg-slate-800 w-full mt-6">
+                <div className="flex">
+                    <CardHeader>
+                        <CardTitle className="text-gray-500 dark:text-white">chasseron average</CardTitle>
+                    </CardHeader>
+                    <div className="mt-6 flex">
+                        <div>
+                            <p className="font-bold text-2xl ml-32 dark:text-white">
+                                {12}°C
+                            </p>
+                        </div>
+                        <div>
+                            <p className="font-bold text-right ml-4 text-2xl dark:text-white">
+                                {58}%
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </Card>
+        </>
+    )
+}
