d85de2d897e3f64e0ea4b263e41c10f301903adf
Nils Hofstetter <119439839+Rignchen@users.noreply.github.com>
2024-06-07 08:38:23 +0200

    feat: get the ip address when averaging the values with the avg_date function (#21)
    * !feat: get ip in the sql average function ouput
    
    * feat: get count of entries in average function output
    
    now that data are separated from the other, some may accidentaly be more important than other ones when trying to get the global average, with the count as an information it is possible to weight the outputs to ensure none of them are more important

--- database/db.sql
+++ database/db.sql
@@ -17,20 +17,20 @@ grant web_anon to authenticator;
 
 -- convert unix timestamp to timestamp and insert it
 create or replace function api.insert_data(
-	temperature real,
-	humidity real,
-	ip varchar(15),
-	unix_timestamp bigint
+    temperature real,
+    humidity real,
+    ip varchar(15),
+    unix_timestamp bigint
 ) 
 returns void as $$
 begin
-	-- if the ip said in the token is not the same as the one in the request, then this means that the user is not who he pretends to be and we should throw an error
-	if (current_setting('request.jwt.claims', true)::json->>'ip' != ip) then
-		raise exception insufficient_privilege
-			using hint = 'You are not who you pretend to be';
-	end if;
-	-- insert the data
-	insert into api.data("temperature", "humidity", "ip", "timestamp") values (temperature, humidity, ip, to_timestamp(unix_timestamp));
+    -- if the ip said in the token is not the same as the one in the request, then this means that the user is not who he pretends to be and we should throw an error
+    if (current_setting('request.jwt.claims', true)::json->>'ip' != ip) then
+        raise exception insufficient_privilege
+            using hint = 'You are not who you pretend to be';
+    end if;
+    -- insert the data
+    insert into api.data("temperature", "humidity", "ip", "timestamp") values (temperature, humidity, ip, to_timestamp(unix_timestamp));
 end $$ language plpgsql;
 
 -- create role for the esp 32
@@ -41,9 +41,9 @@ grant esp32 to authenticator;
 
 -- create a table to store the users
 create table if not exists api.users (
-	id serial primary key,
-	username character varying(50) NOT NULL UNIQUE,
-	password character varying(60) NOT NULL
+    id serial primary key,
+    username character varying(50) NOT NULL UNIQUE,
+    password character varying(60) NOT NULL
 );
 
 -- create role for the users
@@ -65,21 +65,25 @@ insert into api.users(username, password) values ('admin', '$2y$10$vJMf8H4u0f913
 
 -- create a view to get the average temperature and humidity of each day
 create or replace function api.avg_date(
-	delta varchar
+    delta varchar
 )
 returns table(
-	avg_temperature double precision, 
-	avg_humidity double precision, 
-	date timestamp
+    avg_temperature double precision, 
+    avg_humidity double precision, 
+    date timestamp,
+    ip character varying(14),
+    count bigint
 ) as $$
 begin
-	return query select 
-		avg(temperature) as avg_temperature, 
-		avg(humidity) as avg_humidity,
-		date_trunc(delta, timestamp) as date
-	from api.data
-	group by date
-	order by date;
+    return query select 
+        avg(temperature) as avg_temperature, 
+        avg(humidity) as avg_humidity,
+        date_trunc(delta, timestamp) as date,
+        data.ip,
+        count(*) as count
+    from api.data
+    group by date, data.ip
+    order by date;
 end;
 $$ language plpgsql;
 
