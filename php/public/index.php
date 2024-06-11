7f227f66c82247089990309292a4ee90b2aa6d32
Rignchen <rinchendemort@gmail.com>
2024-06-11 09:25:55 +0200

    feat: setup a router

---
+++ php/public/index.php
@@ -0,0 +1,12 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+$app->addRoutingMiddleware();
+$app->addErrorMiddleware(true, true, true);
+
+$app->run();
