72135fd086328ded5ca6e9e20f0545ab94c126f6
Rignchen <rinchendemort@gmail.com>
2024-06-14 10:53:12 +0200

    feat: serve the web pages using the router

--- php/public/index.php
+++ php/public/index.php
@@ -9,4 +9,15 @@ $app = AppFactory::create();
 $app->addRoutingMiddleware();
 $app->addErrorMiddleware(true, true, true);
 
+# the router is use to require the php files
+$app->post('/login', function (Request $request, Response $response, $args) {
+    require __DIR__ . '/../private/login.php';
+    return $response;
+});
+$app->post('/esp', function (Request $request, Response $response, $args) {
+    require __DIR__ . '/../private/esp.php';
+    return $response;
+});
+
 $app->run();
+
