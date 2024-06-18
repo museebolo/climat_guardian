4f99fccd5016048497cfbe43343c473eddcee63e
Alex-zReeZ <alexbeck220@gmail.com>
2024-06-18 13:12:03 +0200

    refactor: renamed file type and function for better separation

--- nextjs-interface/src/app/ui/dashboard/PieChartTemperature.tsx
+++ nextjs-interface/src/app/ui/dashboard/PieChartTemperature.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { getColor } from "@/script/temperatureColor";
+import { getTempColor } from "@/script/getColor";
 import { Pie, PieChart } from "recharts";
 import React from "react";
 
@@ -15,21 +15,21 @@ interface CustomizedLabelProps {
 }
 
 export function PieChartTemperature({ data }: { data: any }) {
-  let newColor = getColor(data[0].value);
+  let newColor = getTempColor(data[0].value);
 
   // Fonction pour rendre le libellé au centre du cercle
   const renderCustomizedLabel = ({ cx, cy, index }: CustomizedLabelProps) => {
     return (
-      <text
-        x={cx}
-        y={cy}
-        fill="black"
-        textAnchor="middle"
-        dominantBaseline="central"
-        fontSize={24}
-      >
-        {`${data[index].value} °C`}
-      </text>
+        <text
+            x={cx}
+            y={cy}
+            fill="black"
+            textAnchor="middle"
+            dominantBaseline="central"
+            fontSize={24}
+        >
+          {`${data[index].value} °C`}
+        </text>
     );
   };
   const calculateEndAngle = (temperature: number) => {
@@ -37,25 +37,25 @@ export function PieChartTemperature({ data }: { data: any }) {
   };
 
   return (
-    <div className="flex h-full w-full flex-col justify-center">
-      <h2 className="pr-5">Température :</h2>
-      <PieChart width={200} height={200}>
-        <Pie
-          innerRadius={60}
-          dataKey="value"
-          startAngle={270}
-          endAngle={calculateEndAngle(data[0].value)}
-          data={data}
-          cx={100}
-          cy={110}
-          outerRadius={80}
-          fill={newColor}
-          label={renderCustomizedLabel}
-          labelLine={false}
-        />
-        <stop offset="5%" stopColor={"#FFF"} stopOpacity={0.8} />
-        <stop offset="95%" stopColor={newColor} stopOpacity={0} />
-      </PieChart>
-    </div>
+      <div className="flex h-full w-full flex-col justify-center">
+        <h2 className="pr-5">Température :</h2>
+        <PieChart width={200} height={200}>
+          <Pie
+              innerRadius={60}
+              dataKey="value"
+              startAngle={270}
+              endAngle={calculateEndAngle(data[0].value)}
+              data={data}
+              cx={100}
+              cy={110}
+              outerRadius={80}
+              fill={newColor}
+              label={renderCustomizedLabel}
+              labelLine={false}
+          />
+          <stop offset="5%" stopColor={"#FFF"} stopOpacity={0.8} />
+          <stop offset="95%" stopColor={newColor} stopOpacity={0} />
+        </PieChart>
+      </div>
   );
 }
