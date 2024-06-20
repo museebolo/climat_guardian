abf50c8fdc220a955cc25e68d23f916445d8aa26
jordybsk <dylan.bossoku@jobtrek.ch>
2024-06-20 08:28:54 +0200

    feat: add ip parameter in useLastData

--- nextjs-interface/src/lib/data.ts
+++ nextjs-interface/src/lib/data.ts
@@ -1,12 +1,7 @@
 import { useEffect, useState } from "react";
-import {
-  SampleContext,
-  getToken,
-  data,
-  avgData,
-} from "@/lib/context";
+import { SampleContext, getToken, data, avgData } from "@/lib/context";
 
-export const useFetchData = (precision: string,ip:string) => {
+export const useFetchData = (precision: string, ip: string) => {
   const [data, setData] = useState<avgData[]>([]);
 
   useEffect(() => {
@@ -23,11 +18,11 @@ export const useFetchData = (precision: string,ip:string) => {
   return data;
 };
 
-export function useLastData(type: string) {
+export function useLastData(type: string, ip: string) {
   const [temperature, setTemperature] = useState<number | undefined>(undefined);
 
   useEffect(() => {
-    const url = `${SampleContext.urlData}/data?limit=1&order=timestamp.desc`;
+    const url = `${SampleContext.urlData}/data?limit=1&order=timestamp.desc&ip=eq.${ip}`;
     fetch(url, { headers: { Authorization: `Bearer ${getToken()}` } })
       .then((response) => response.json())
       .then((apiData: data[]) => {
@@ -40,6 +35,6 @@ export function useLastData(type: string) {
       .catch((e) => {
         console.error("Une erreur s'est produite :", e);
       });
-  }, [type]);
+  }, [ip, type]);
   return temperature;
 }
