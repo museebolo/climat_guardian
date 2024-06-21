bd408cddc989502b4e852ed2431dafa35093c5fb
Alex-zReeZ <alexbeck220@gmail.com>
2024-06-21 15:56:23 +0200

    fix: fixed function in map

--- nextjs-interface/src/app/dashboard/page.tsx
+++ nextjs-interface/src/app/dashboard/page.tsx
@@ -4,12 +4,10 @@
 import { useState, useEffect } from "react";
 
 // import components
-import { PieChartTemperature } from "@/app/ui/dashboard/PieChartTemperature";
-import { PieChartHumidity } from "@/app/ui/dashboard/PieChartHumidity";
+import DataCircle from "@/app/ui/dashboard/DataCircle";
 
 // import functions
 import { useRouter } from "next/navigation";
-import { useLastData} from "@/lib/data";
 
 
 const ESPList = [
@@ -34,42 +32,13 @@ export default function Page() {
     }
   }, [router]);
 
-  /*
-  // get ip from esp name and fetch data
-  const ip = "172.16.4.100";
-
-
-  // get last data for temperature and humidity
-  const temperature = useLastData("temperature", ip);
-  const humidity = useLastData("humidity", ip);
-*/
-
   return (
       <>
         <div className="px-auto grid h-fit w-full min-w-[500px] grid-cols-1 gap-10 xl:grid-cols-2 2xl:grid-cols-3">
-          {ESPList.map((esp, index) => {
-            // get ip from esp name and fetch data
-            const ip = esp.ip;
+          {ESPList.map((esp, index) => (
+              <DataCircle key={index} esp={esp} />
 
-            // get last data for temperature and humidity
-            const temperature = useLastData("temperature", ip);
-            const humidity = useLastData("humidity", ip);
-
-            return (
-                <div
-                    className="flex h-full flex-col items-center rounded-2xl border-2 text-center"
-                    key={index}
-                >
-                  <h2 className="w-full border-b-2 pb-5 pt-5 text-center text-gray-800">
-                    {esp.name}
-                  </h2>
-                  <div className="sm:py-auto flex h-full w-full flex-row py-14">
-                    <PieChartTemperature data={temperature} />
-                    <PieChartHumidity data={humidity} />
-                  </div>
-                </div>
-            );
-          })}
+          ))}
         </div>
       </>
   );
