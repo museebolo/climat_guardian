4197a28c16567bfdde01ed9f95d2fdf239caff7c
Alex-zReeZ <145986307+Alex-zReeZ@users.noreply.github.com>
2024-06-25 08:43:11 +0200

    refactor: Rounded data  (#42)
    * refactor: moved file position
    
    * fix: fixed import errors
    
    * refactor: rounded data to two decimal
    
    * refactor: reformated files
    
    * refactor: translated comment in english
    
    ---------
    
    Co-authored-by: jordyBSK <125542402+jordyBSK@users.noreply.github.com>

--- nextjs-interface/src/app/dashboard/page.tsx
+++ nextjs-interface/src/app/dashboard/page.tsx
@@ -9,14 +9,13 @@ import DataCircle from "@/app/ui/dashboard/DataCircle";
 // import functions
 import { useRouter } from "next/navigation";
 
-
 const ESPList = [
-  { name: "ESP N°1", ip:"172.16.5.178" },
-  { name: "ESP N°2" ,ip:"172.16.4.100"},
-  { name: "ESP N°3" ,ip:"172.16.5.65"},
-  { name: "ESP N°4", ip:"172.16.5.178" },
-  { name: "ESP N°5", ip:"172.16.4.100" },
-  { name: "ESP N°6", ip:"172.16.5.65"},
+  { name: "ESP N°1", ip: "172.16.5.178" },
+  { name: "ESP N°2", ip: "172.16.4.100" },
+  { name: "ESP N°3", ip: "172.16.5.178" },
+  { name: "ESP N°4", ip: "172.16.4.100" },
+  { name: "ESP N°5", ip: "172.16.5.178" },
+  { name: "ESP N°6", ip: "172.16.4.100" },
 ];
 
 export default function Page() {
@@ -34,13 +33,12 @@ export default function Page() {
   }, [router]);
 
   return (
-      <>
-        <div className="px-auto grid h-fit w-full min-w-[500px] grid-cols-1 gap-10 xl:grid-cols-2 2xl:grid-cols-3">
-          {ESPList.map((esp, index) => (
-              <DataCircle key={index} esp={esp} />
-
-          ))}
-        </div>
-      </>
+    <>
+      <div className="px-auto grid h-fit w-full min-w-[500px] grid-cols-1 gap-10 xl:grid-cols-2 2xl:grid-cols-3">
+        {ESPList.map((esp, index) => (
+          <DataCircle key={index} esp={esp} />
+        ))}
+      </div>
+    </>
   );
 }
