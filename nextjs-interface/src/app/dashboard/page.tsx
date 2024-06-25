a1c8f8db5a81aa289cc734b6e74a131993c01db5
jordyBSK <125542402+jordyBSK@users.noreply.github.com>
2024-06-25 15:53:35 +0200

    Revert "feat: push production ready version to main (#52)"
    This reverts commit 366d14cdd9f903b544a3974f0fd1fc336832e371.

--- nextjs-interface/src/app/dashboard/page.tsx
+++ nextjs-interface/src/app/dashboard/page.tsx
@@ -1,24 +1,46 @@
 "use client";
+
+// import hooks
+import { useState, useEffect } from "react";
+
 // import components
 import DataCircle from "@/app/ui/dashboard/DataCircle";
 
+// import functions
+import { useRouter } from "next/navigation";
+
+
 const ESPList = [
-  { name: "ESP N°1", ip: "172.16.5.178" },
-  { name: "ESP N°2", ip: "172.16.4.100" },
-  { name: "ESP N°3", ip: "172.16.5.178" },
-  { name: "ESP N°4", ip: "172.16.4.100" },
-  { name: "ESP N°5", ip: "172.16.5.178" },
-  { name: "ESP N°6", ip: "172.16.4.100" },
+  { name: "ESP N°1", ip:"172.16.5.178" },
+  { name: "ESP N°2" ,ip:"172.16.4.100"},
+  { name: "ESP N°3" ,ip:"172.16.5.178"},
+  { name: "ESP N°4", ip:"172.16.4.100" },
+  { name: "ESP N°5", ip:"172.16.5.178" },
+  { name: "ESP N°6", ip:"172.16.4.100"},
 ];
 
 export default function Page() {
+  const [token, setToken] = useState<string | null>(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    const storedToken = window.localStorage.getItem("token");
+    if (!storedToken) {
+      router.push("/login");
+    } else {
+      setToken(storedToken);
+      router.push("/dashboard");
+    }
+  }, [router]);
+
   return (
-    <>
-      <div className="px-auto grid h-fit w-full min-w-[500px] grid-cols-1 gap-10 xl:grid-cols-2 2xl:grid-cols-3">
-        {ESPList.map((esp, index) => (
-          <DataCircle key={index} esp={esp} />
-        ))}
-      </div>
-    </>
+      <>
+        <div className="px-auto grid h-fit w-full min-w-[500px] grid-cols-1 gap-10 xl:grid-cols-2 2xl:grid-cols-3">
+          {ESPList.map((esp, index) => (
+              <DataCircle key={index} esp={esp} />
+
+          ))}
+        </div>
+      </>
   );
 }
--- nextjs-interface/src/app/ui/dashboard/DataCircle.tsx
+++ nextjs-interface/src/app/ui/dashboard/DataCircle.tsx
@@ -1,21 +1,21 @@
-import { useLastData } from "@/lib/data";
-import { PieChartTemperature } from "@/app/ui/dashboard/PieChartTemperature";
-import { PieChartHumidity } from "@/app/ui/dashboard/PieChartHumidity";
+import {useLastData} from "@/lib/data";
+import {PieChartTemperature} from "@/app/ui/dashboard/PieChartTemperature";
+import {PieChartHumidity} from "@/app/ui/dashboard/PieChartHumidity";
 
 export default function DataCircle({ esp }: { esp: any }) {
-  const ip = esp.ip;
-  const temperature = useLastData("temperature", ip);
-  const humidity = useLastData("humidity", ip);
+    const ip = esp.ip;
+    const temperature = useLastData("temperature", ip);
+    const humidity = useLastData("humidity", ip);
 
-  return (
-    <div className="flex h-full flex-col items-center rounded-2xl border-2 text-center">
-      <h2 className="w-full border-b-2 pb-5 pt-5 text-center text-gray-800">
-        {esp.name}
-      </h2>
-      <div className="sm:py-auto flex h-full w-full flex-row py-14">
-        <PieChartTemperature data={temperature} />
-        <PieChartHumidity data={humidity} />
-      </div>
-    </div>
-  );
+    return (
+        <div className="flex h-full flex-col items-center rounded-2xl border-2 text-center">
+            <h2 className="w-full border-b-2 pb-5 pt-5 text-center text-gray-800">
+                {esp.name}
+            </h2>
+            <div className="sm:py-auto flex h-full w-full flex-row py-14">
+                <PieChartTemperature data={temperature} />
+                <PieChartHumidity data={humidity} />
+            </div>
+        </div>
+    );
 }
