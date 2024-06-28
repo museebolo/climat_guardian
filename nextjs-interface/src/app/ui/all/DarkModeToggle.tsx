4781dd337c1dd67537e7794f5488a005155104c1
Alex-zReeZ <alexbeck220@gmail.com>
2024-06-28 15:24:12 +0200

    style: reformated file and changed color on dark theme

--- nextjs-interface/src/app/ui/all/DarkModeToggle.tsx
+++ nextjs-interface/src/app/ui/all/DarkModeToggle.tsx
@@ -1,19 +1,22 @@
 // DarkModeToggle.tsx
-import React from 'react';
+import React from "react";
 import { Moon, Sun } from "lucide-react";
-
 
 const DarkModeToggle = () => {
   const [darkMode, setDarkMode] = React.useState(false);
 
   React.useEffect(() => {
     const classList = document.body.classList;
-    darkMode ? classList.add('dark') : classList.remove('dark');
+    darkMode ? classList.add("dark") : classList.remove("dark");
   }, [darkMode]);
 
   return (
-    <div onClick={() => setDarkMode(!darkMode)} className="pr-14" >
-      {darkMode ? <Sun className="bg-secondary w-full h-full  p-2 rounded-full transition"/> : <Moon className="bg-secondary w-full h-full  p-2 rounded-full" />}
+    <div onClick={() => setDarkMode(!darkMode)} className="pr-14">
+      {darkMode ? (
+        <Sun className="h-full w-full rounded-full bg-secondary p-2 transition" />
+      ) : (
+        <Moon className="h-full w-full rounded-full bg-secondary p-2" />
+      )}
     </div>
   );
 };
