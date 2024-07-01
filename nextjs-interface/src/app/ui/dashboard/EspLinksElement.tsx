d4639876905dd027022cb459e600a720c450da1b
jordyBSK <125542402+jordyBSK@users.noreply.github.com>
2024-07-01 08:59:02 +0200

    Middleware (#64)
    * refactor: removed redirection in getToken()
    
    * refactor: removed login redirection in /
    
    * refactor: add token in cookies
    
    * feat: implement middleware
    
    ---------
    
    Co-authored-by: Alex-zReeZ <145986307+Alex-zReeZ@users.noreply.github.com>

--- nextjs-interface/src/app/ui/dashboard/EspLinksElement.tsx
+++ nextjs-interface/src/app/ui/dashboard/EspLinksElement.tsx
@@ -1,12 +1,8 @@
 "use client";
-
 import { useState } from "react";
-
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-
 import clsx from "clsx";
-
 import {
   Popover,
   PopoverContent,
