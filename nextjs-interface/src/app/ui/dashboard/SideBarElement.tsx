b60c22ae2c9a1fa4ce569178f846975d545e2f95
Alex-zReeZ <alexbeck220@gmail.com>
2024-07-02 13:58:42 +0200

    refactor: simplified sidebar code

--- nextjs-interface/src/app/ui/dashboard/SideBarElement.tsx
+++ nextjs-interface/src/app/ui/dashboard/SideBarElement.tsx
@@ -4,15 +4,11 @@ import EspLinksElement from "@/app/ui/dashboard/EspLinksElement";
 
 export default function SideBarElement() {
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      <div className="hidden border-r bg-muted/40 md:block">
-        <div className="flex h-full max-h-screen flex-col gap-2 px-4 pt-2 lg:px-6">
-          <div className="">
-            <NavLinksElement />
-            <EspLinksElement />
+      <div className="hidden md:flex flex-col m-5 mt-0 border-r-2 border-b-2 h-full min-h-screen">
+          <div>
+              <NavLinksElement />
+              <EspLinksElement />
           </div>
-        </div>
       </div>
-    </div>
   );
 }
