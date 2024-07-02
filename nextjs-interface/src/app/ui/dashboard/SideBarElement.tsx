2a9a89d067d2c7f83c045eb1185e62795c9db6f6
Alex-zReeZ <alexbeck220@gmail.com>
2024-07-02 14:40:43 +0200

    refactor: added padding to add air to sidebar

--- nextjs-interface/src/app/ui/dashboard/SideBarElement.tsx
+++ nextjs-interface/src/app/ui/dashboard/SideBarElement.tsx
@@ -5,7 +5,7 @@ import EspLinksElement from "@/app/ui/dashboard/EspLinksElement";
 export default function SideBarElement() {
   return (
       <div className="hidden md:flex flex-col mr-0 ml-0 border-r-2 border-b-2 h-full min-h-screen">
-          <div>
+          <div className="pr-5">
               <NavLinksElement />
               <EspLinksElement />
           </div>
