f6b0e6de76b478ff573fbea638818842ef1ec04c
Alex-zReeZ <alexbeck220@gmail.com>
2024-07-04 14:19:55 +0200

    test:  fix nginx config and broke interface to test fail

--- nextjs-interface/src/app/ui/all/providers.tsx
+++ nextjs-interface/src/app/ui/all/providers.tsx
@@ -1,31 +1,25 @@
 "use client";
-
-// import react libraries
 import { useState } from "react";
-
-// import theme context
 import { ThemeContext } from "@/lib/Theme";
 
-// import scripts
-import { getCookie, setCookie } from "@/lib/cookies";
-
-export function Providers({ children }: { children: React.ReactNode }) {
-  // set dark mode
+export function Providers({
+                            children,
+                          }: Readonly<{
+  children: React.ReactNode;
+}>) {
   const [darkMode, setDarkMode] = useState(() => {
-    const savedMode = getCookie("darkMode");
-    return savedMode ? savedMode === "true" : false;
+    const savedMode = localStorage.getItem("darkMode");
+    return savedMode ? JSON.parse(savedMode) : false;
   });
-
-  // toggle dark mode
   const toggleDarkMode = () => {
     const newDarkMode = !darkMode;
     setDarkMode(newDarkMode);
-    setCookie({ name: "darkMode", value: newDarkMode });
+    localStorage.setItem("darkMode", JSON.stringify(newDarkMode));
   };
 
   return (
-    <ThemeContext.Provider value={{ darkMode, toggleDarkMode }}>
-      {children}
-    </ThemeContext.Provider>
+      <ThemeContext.Provider value={{ darkMode, toggleDarkMode }}>
+        {children}
+      </ThemeContext.Provider>
   );
 }
