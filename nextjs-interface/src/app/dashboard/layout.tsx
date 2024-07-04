0ea1cec4e1df2712f6dd990fcd08bc92d2b3ddc4
jordybsk <dylan.bossoku@jobtrek.ch>
2024-07-04 09:19:06 +0200

    style: change background dark mode color

--- nextjs-interface/src/app/dashboard/layout.tsx
+++ nextjs-interface/src/app/dashboard/layout.tsx
@@ -7,11 +7,11 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="w-full dark:bg-slate-700">
         <HeaderElement />
       </div>
-      <div className="flex flex-row w-full pb-5 lg:pl-0 pl-5 pr-5 gap-x-5 dark:bg-slate-800">
+      <div className="flex flex-row w-full pb-5 lg:pl-0 pl-5 pr-5 gap-x-5 dark:bg-slate-900">
         <div className="hidden lg:block pl-5 w-1/6 dark:bg-slate-700">
           <SideBarElement />
         </div>
-        <div className="w-full pt-5 lg:w-5/6 dark:bg-slate-800">
+        <div className="w-full pt-5 lg:w-5/6 dark:bg-slate-900">
             {children}
         </div>
       </div>
