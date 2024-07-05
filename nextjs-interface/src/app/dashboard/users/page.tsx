ba244207ec537b18eddac733fae09a5b709c96b6
jordybsk <dylan.bossoku@jobtrek.ch>
2024-07-05 16:24:49 +0200

    style: reformat code

--- nextjs-interface/src/app/dashboard/users/page.tsx
+++ nextjs-interface/src/app/dashboard/users/page.tsx
@@ -1,15 +1,16 @@
 "use client";
-import { useState, useEffect } from 'react';
-import { useAllUsers } from '@/lib/data';
-import { User, Trash2 } from 'lucide-react';
-import { AddUserElement } from '@/app/ui/dashboard/AddUserElement';
+import { useState, useEffect } from "react";
+import { useAllUsers } from "@/lib/data";
+import { User, Trash2 } from "lucide-react";
+import { AddUserElement } from "@/app/ui/dashboard/AddUserElement";
 import {
   Card,
-  CardContent, CardFooter,
+  CardContent,
+  CardFooter,
   CardHeader,
   CardTitle,
-} from '@/components/ui/card';
-import { getToken, user } from '@/lib/context';
+} from "@/components/ui/card";
+import { getToken, user } from "@/lib/context";
 
 export default function Page() {
   const [users, setUsers] = useState<user[]>([]);
@@ -28,56 +29,55 @@ export default function Page() {
   const handleDelete = async (username: string) => {
     try {
       const response = await fetch(`/postgrest/users?username=eq.${username}`, {
-        method: 'DELETE',
+        method: "DELETE",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
           Authorization: `Bearer ${getToken()}`,
         },
       });
 
       if (!response.ok) {
-        console.error('Failed to delete user');
+        console.error("Failed to delete user");
         console.error(await response.json());
         return;
       }
 
       // Remove user from local state after successful deletion
-      setUsers(users.filter(user => user.username !== username));
-
+      setUsers(users.filter((user) => user.username !== username));
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   };
 
   return (
-      <>
-        <Card>
-          <CardHeader>
-            <CardTitle>All Users</CardTitle>
-          </CardHeader>
-          <CardContent>
-            {users.map((user, index) => (
-                <div
-                    className="flex items-center justify-between mb-4 border-b border-gray-200"
-                    key={index}
-                >
-                  <div className="flex items-center gap-4 mb-4">
-                    <User />
-                    <span className="text-lg font-bold">{user.username}</span>
-                  </div>
-                  <div>
-                    <Trash2
-                        className="delete-icon cursor-pointer"
-                        onClick={() => handleDelete(user.username)}
-                    />
-                  </div>
-                </div>
-            ))}
-          </CardContent>
-          <CardFooter>
-            <AddUserElement setUsers={setUsers} users={users} />
-          </CardFooter>
-        </Card>
-      </>
+    <>
+      <Card>
+        <CardHeader>
+          <CardTitle>All Users</CardTitle>
+        </CardHeader>
+        <CardContent>
+          {users.map((user, index) => (
+            <div
+              className="mb-4 flex items-center justify-between border-b border-gray-200"
+              key={index}
+            >
+              <div className="mb-4 flex items-center gap-4">
+                <User />
+                <span className="text-lg font-bold">{user.username}</span>
+              </div>
+              <div>
+                <Trash2
+                  className="delete-icon cursor-pointer"
+                  onClick={() => handleDelete(user.username)}
+                />
+              </div>
+            </div>
+          ))}
+        </CardContent>
+        <CardFooter>
+          <AddUserElement setUsers={setUsers} users={users} />
+        </CardFooter>
+      </Card>
+    </>
   );
 }
--- nextjs-interface/src/app/ui/dashboard/AddUserElement.tsx
+++ nextjs-interface/src/app/ui/dashboard/AddUserElement.tsx
@@ -4,69 +4,77 @@ import { Button } from "@/components/ui/button";
 import { getToken, user } from "@/lib/context";
 import bcrypt from "bcryptjs";
 
-export function AddUserElement({ users, setUsers }: { users: user[], setUsers: Dispatch<SetStateAction<user[]>> }) {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [message, setMessage] = useState("");
+export function AddUserElement({
+  users,
+  setUsers,
+}: {
+  users: user[];
+  setUsers: Dispatch<SetStateAction<user[]>>;
+}) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
 
-    const handleAddUser = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        e.preventDefault();
+  const handleAddUser = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    e.preventDefault();
 
-        // Hash the password
-        const hashedPassword:string = await bcrypt.hash(password, 10);
-        const token = getToken();
+    // Hash the password
+    const hashedPassword: string = await bcrypt.hash(password, 10);
+    const token = getToken();
 
-        try {
-            const response = await fetch("http://localhost/postgrest/users", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${token}`,
-                },
-                body: JSON.stringify({
-                    username,
-                    password: hashedPassword,
-                }),
-            });
+    try {
+      const response = await fetch("http://localhost/postgrest/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          username,
+          password: hashedPassword,
+        }),
+      });
 
-            if (response.ok) {
-                const newUser: user = { username, password: hashedPassword };
-                setUsers([...users, newUser]);
-                setMessage("User added successfully!");
-                setUsername("");
-                setPassword("");
-            } else {
-                setMessage("Failed to add user. Please try again.");
-            }
-        } catch (error: any) {
-            setMessage("Error: " + error.message);
-        }
-    };
+      if (response.ok) {
+        const newUser: user = { username, password: hashedPassword };
+        setUsers([...users, newUser]);
+        setMessage("User added successfully!");
+        setUsername("");
+        setPassword("");
+      } else {
+        setMessage("Failed to add user. Please try again.");
+      }
+    } catch (error: any) {
+      setMessage("Error: " + error.message);
+    }
+  };
 
-    return (
-        <div>
-            <div className="flex">
-                <Input
-                    className="w-72"
-                    placeholder="Username"
-                    type="text"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    required
-                />
+  return (
+    <div>
+      <div className="flex">
+        <Input
+          className="w-72"
+          placeholder="Username"
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
 
-                <Input
-                    className="w-72"
-                    placeholder="Password"
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
+        <Input
+          className="w-72"
+          placeholder="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
 
-                <Button onClick={handleAddUser}>Add User</Button>
-            </div>
-            {message && <p className="text-emerald-600 mt-6">{message}</p>}
-        </div>
-    );
+        <Button onClick={handleAddUser}>Add User</Button>
+      </div>
+      {message && <p className="mt-6 text-emerald-600">{message}</p>}
+    </div>
+  );
 }
