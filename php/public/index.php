33b51c04db61a60317eb088e8916c11b258a1816
Rignchen <rinchendemort@gmail.com>
2024-07-05 13:31:59 +0200

    refactor: move login inside a class

--- php/public/index.php
+++ php/public/index.php
@@ -6,15 +6,13 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
+// Set up the router
 $app = AppFactory::create();
 $app->addRoutingMiddleware();
 $app->addErrorMiddleware(true, true, true);
 
-# the router is use to require the php files
-$app->post('/login', function (Request $request, Response $response, $args) {
-    require __DIR__ . '/../private/login.php';
-    return $response;
-});
+# the router is used to require the php files
+$app->post('/login', App\Login::class);
 $app->post('/esp', function (Request $request, Response $response, $args) {
     require __DIR__ . '/../private/esp.php';
     return $response;
---
+++ php/src/Login.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Firebase\JWT\JWT;
+
+// Load environment variables
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__ . '/../.env');
+
+class Login {
+    public function __invoke(Request $request, Response $response, mixed $args): Response {
+
+        $data = json_decode(file_get_contents("php://input"), true);
+
+        if (!isset($data['username']) || !isset($data['password']))
+            return output($response, ['error' => 'Username and password are required'], 400);
+
+        // Generate a token and use it to get the user
+        $token = JWT::encode(['role' => 'web_login', 'exp' => time()], $_ENV['JWT_SECRET'], 'HS256');
+        $user = callAPI('GET', $_ENV['POSTGREST_API'] . "/users?username=eq.{$data['username']}&limit=1&select=password,id", [], ["Authorization: Bearer $token"]);
+
+        // Check if the answer is valid
+        if ($user === false)
+            return output($response, ['error' =>
+                $_ENV['DETAILED_ERRORS'] === 'true' ? 
+                    'Unable to connect to the API' :
+                    'Unknown error'
+            ], 500);
+        $user = json_decode($user, true);
+        if (isset($user["message"]))
+            return output($response, ['error' =>
+                $_ENV['DETAILED_ERRORS'] === 'true' ? 
+                    $user :
+                    'Unknown error'
+            ], 500);
+
+        if (empty($user))
+            return output($response, ['error' => 'Unknown user'], 401);
+        if (!password_verify($data['password'], $user[0]['password']))
+            return output($response, ['error' => 'Invalid password'], 401);
+
+        // Generate a token for the user that expires at midnight
+        $payload = [
+            'role' => 'web_user',
+            'id' => $user[0]['id'],
+            'exp' => strtotime('tomorrow midnight')
+        ];
+        $token = JWT::encode($payload, $_ENV['JWT_SECRET'], 'HS256');
+
+        return output($response, ['token' => $token]);
+    }
+}
+
