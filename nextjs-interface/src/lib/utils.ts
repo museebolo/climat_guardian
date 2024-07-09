d4f151724965b1876901dc47221ac12ac15ed6c7
Alex-zReeZ <alexbeck220@gmail.com>
2024-07-09 13:48:52 +0200

    refactor: added verification of ip in js

--- nextjs-interface/src/lib/utils.ts
+++ nextjs-interface/src/lib/utils.ts
@@ -15,3 +15,8 @@ export const copyToClipboard = (text: string) => {
       console.error("Failed to copy token: ", err);
     });
 };
+
+export function validateIp(ip: string): boolean {
+  const ipRegex = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+  return ipRegex.test(ip);
+}
