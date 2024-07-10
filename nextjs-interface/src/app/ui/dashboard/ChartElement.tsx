60ac93dbcc0fa4deba442a842b81717975df809d
Alex-zReeZ <alexbeck220@gmail.com>
2024-07-10 08:43:53 +0200

    style: reformated file and removed unused import

--- nextjs-interface/src/app/ui/dashboard/ChartElement.tsx
+++ nextjs-interface/src/app/ui/dashboard/ChartElement.tsx
@@ -8,7 +8,6 @@ import {
   Legend,
   ReferenceLine,
   ResponsiveContainer,
-  CartesianGrid,
 } from "recharts";
 import { avgData } from "@/lib/context";
 import { ThemeContext } from "@/lib/Theme";
@@ -19,91 +18,101 @@ export function ChartElement({ data }: { data: avgData[] }) {
   let textColor = darkMode ? "white" : "";
 
   if (!Array.isArray(data) || data === undefined) {
-    console.error('Data is not an array or is undefined');
+    console.error("Data is not an array or is undefined");
     return null;
   }
 
   const dateChanges = data.reduce((acc: any[], curr, index, src) => {
-    if (index > 0 && new Date(curr.date).getDate() !== new Date(src[index - 1].date).getDate()) {
+    if (
+      index > 0 &&
+      new Date(curr.date).getDate() !== new Date(src[index - 1].date).getDate()
+    ) {
       acc.push(curr.date);
     }
     return acc;
   }, []);
 
   return (
-      <ResponsiveContainer width="100%" height={500}>
-        <LineChart width={800} height={500} data={data}>
-          <XAxis
-              fontSize={16}
-              tickLine={false}
-              axisLine={false}
-              dataKey="date"
-              stroke={textColor}
-              tickFormatter={(value: string) =>
-                  new Date(value).toLocaleDateString()
-              }
-              padding={{ left: 20, right: 50 }}
+    <ResponsiveContainer width="100%" height={500}>
+      <LineChart width={800} height={500} data={data}>
+        <XAxis
+          fontSize={16}
+          tickLine={false}
+          axisLine={false}
+          dataKey="date"
+          stroke={textColor}
+          tickFormatter={(value: string) =>
+            new Date(value).toLocaleDateString()
+          }
+          padding={{ left: 20, right: 50 }}
+        />
+        <YAxis
+          fontSize={16}
+          tickLine={false}
+          axisLine={false}
+          stroke={textColor}
+          padding={{ top: 50, bottom: 10 }}
+        />
+        <Tooltip
+          cursor={{
+            stroke: "#334155",
+            radius: 4,
+          }}
+          content={({ active, payload }) => {
+            if (!active || !payload || payload.length === 0) return null;
+            return (
+              <>
+                <div className="w-[250px] rounded-xl bg-secondary dark:bg-gray-900">
+                  <div className="p-5">
+                    <div className="flex flex-col">
+                      <h1 className="text-black dark:text-zinc-50">
+                        Date :{" "}
+                        {new Date(
+                          payload[0].payload.date,
+                        ).toLocaleDateString() +
+                          " " +
+                          new Date(
+                            payload[0].payload.date,
+                          ).toLocaleTimeString()}
+                      </h1>
+                      <p className="text-black dark:text-zinc-50">
+                        Temperature :{" "}
+                        {payload[0].payload.avg_temperature.toFixed(2)}°C
+                      </p>
+                      <p className="text-black dark:text-zinc-50">
+                        Humidity : {payload[0].payload.avg_humidity.toFixed(2)}%
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </>
+            );
+          }}
+        />
+        <Legend />
+        <Line
+          type="monotone"
+          dataKey="avg_humidity"
+          stroke="#8884d8"
+          activeDot={{ r: 8 }}
+          strokeWidth="2px"
+        />
+        <Line
+          type="monotone"
+          dataKey="avg_temperature"
+          strokeWidth="2px"
+          stroke="#82ca9d"
+        />
+        {dateChanges.map((date, index) => (
+          <ReferenceLine
+            key={index}
+            x={date}
+            stroke="Gray"
+            ifOverflow="extendDomain"
+            label={{ value: "Date change", position: "top", fill: "green" }}
           />
-          <YAxis
-              fontSize={16}
-              tickLine={false}
-              axisLine={false}
-              stroke={textColor}
-              padding={{ top: 50, bottom: 10 }}
-          />
-          <Tooltip
-              cursor={{
-                stroke: "#334155",
-                radius: 4,
-              }}
-              content={({ active, payload }) => {
-                if (!active || !payload || payload.length === 0) return null;
-                return (
-                    <>
-                      <div className="w-[250px] rounded-xl bg-secondary dark:bg-gray-900">
-                        <div className="p-5">
-                          <div className="flex flex-col">
-                            <h1 className="text-black dark:text-zinc-50">
-                              Date :{" "}
-                              {new Date(payload[0].payload.date).toLocaleDateString() + " " + new Date(payload[0].payload.date).toLocaleTimeString()}
-                            </h1>
-                            <p className="text-black dark:text-zinc-50">
-                              Temperature :{" "}
-                              {payload[0].payload.avg_temperature.toFixed(2)}°C
-                            </p>
-                            <p className="text-black dark:text-zinc-50">
-                              Humidity : {payload[0].payload.avg_humidity.toFixed(2)}%
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-                    </>
-                );
-              }}          />
-          <Legend />
-          <Line
-              type="monotone"
-              dataKey="avg_humidity"
-              stroke="#8884d8"
-              activeDot={{ r: 8 }}
-              strokeWidth="2px"
-          />
-          <Line
-              type="monotone"
-              dataKey="avg_temperature"
-              strokeWidth="2px"
-              stroke="#82ca9d"
-          />
-          {dateChanges.map((date, index) => (
-              <ReferenceLine
-                  key={index}
-                  x={date}
-                  stroke="Gray"
-                  ifOverflow="extendDomain"
-                  label={{ value: "Date change", position: "top", fill: "green" }}
-              />
-          ))}
-        </LineChart>
-      </ResponsiveContainer>
+        ))}
+      </LineChart>
+    </ResponsiveContainer>
   );
 }
