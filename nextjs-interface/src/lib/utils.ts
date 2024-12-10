04995507b935e1c3cf3746cda2829ce50000c67a
ColinRgm <colin.regamey@icloud.com>
2024-12-10 10:38:57 +0100

    fix: add a debug alert to see if it works
    Closes: #202

--- nextjs-interface/src/lib/utils.ts
+++ nextjs-interface/src/lib/utils.ts
@@ -1,23 +1,24 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import {type ClassValue, clsx} from "clsx";
+import {twMerge} from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+    return twMerge(clsx(inputs));
 }
 
 export const copyToClipboard = (text: string) => {
-  navigator.clipboard
-    .writeText(text)
-    .then(() => {
-      console.log("Token copié dans le clipboard");
-    })
-    .catch((err) => {
-      console.error("Copie du Token échoué : ", err);
-    });
+    navigator.clipboard
+        .writeText(text)
+        .then(() => {
+            console.log("Token copié dans le clipboard");
+            alert("Token copié")
+        })
+        .catch((err) => {
+            console.error("Copie du Token échoué : ", err);
+        });
 };
 
 export function validateIp(ip: string): boolean {
-  const ipRegex =
-    /^((25[0-5]|2[0-4]\d|[01]?\d{1,2})\.){3}(25[0-5]|2[0-4]\d|[01]?\d{1,2})$/;
-  return ipRegex.test(ip);
+    const ipRegex =
+        /^((25[0-5]|2[0-4]\d|[01]?\d{1,2})\.){3}(25[0-5]|2[0-4]\d|[01]?\d{1,2})$/;
+    return ipRegex.test(ip);
 }
