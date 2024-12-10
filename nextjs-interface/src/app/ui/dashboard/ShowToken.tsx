875db148cebd20a9e3c935dbac6356efcf0469d9
ColinRgm <90181373+ColinRgm@users.noreply.github.com>
2024-12-10 15:57:51 +0100

    Bug/copy token (#203)
    * fix: use a hook to get the token when available
    
    Closes: #202
    Closes: #194

--- nextjs-interface/src/app/ui/dashboard/ShowToken.tsx
+++ nextjs-interface/src/app/ui/dashboard/ShowToken.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { copyToClipboard } from "@/lib/utils";
 import { useFetchToken } from "@/lib/data";
 
 export default function ShowToken({ ip }: { ip: string }) {
   const newToken = useFetchToken(ip);
-  copyToClipboard(newToken);
+
+  useEffect(() => {
+    copyToClipboard(newToken);
+  }, [newToken]);
 
   return (
     <div className="mt-2 bg-zinc-200 p-2 text-sm dark:bg-zinc-800">
