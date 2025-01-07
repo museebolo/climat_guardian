6445bdb819c16d274bce8c39f39bffe8c2114586
ColinRgm <colin.regamey@icloud.com>
2025-01-07 11:10:14 +0100

    feat: formating
    Closes: #184

--- nextjs-interface/src/app/ui/dashboard/DeleteEsp.tsx
+++ nextjs-interface/src/app/ui/dashboard/DeleteEsp.tsx
@@ -1,54 +1,45 @@
-import {Trash2} from "lucide-react";
-import {getToken} from "@/lib/context";
+import { Trash2 } from "lucide-react";
+import { getToken } from "@/lib/context";
 
-export default function DeleteEsp({id}: { id: string }) {
+export default function DeleteEsp({ id }: { id: string }) {
+  const deleteEsp = async (id: string) => {
+    // Get the id in the URL of the page
+    const urlData = `/postgrest/data?esp_id=eq.${id}`;
 
-    const deleteEsp = async (id: string) => {
+    const url = `/postgrest/esp?id=eq.${id}`;
 
+    try {
+      const responseData = await fetch(urlData, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${getToken()}`,
+        },
+      });
 
-        // Get the id in the URL of the page
-        const urlData = `/postgrest/data?esp_id=eq.${id}`;
+      const response = await fetch(url, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${getToken()}`,
+        },
+      });
 
-        const url = `/postgrest/esp?id=eq.${id}`;
+      window.location.href = `/dashboard`;
 
+      if (!responseData.ok && !response.ok) {
+        console.error(`une erreur lors de la suppression de l'ESP`);
+        console.error(await responseData.json());
+        console.error(await response.json());
+      }
+    } catch (error) {
+      console.error("Error: ", error);
+    }
+  };
 
-        try {
-            const responseData = await fetch(urlData, {
-                method: "DELETE",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${getToken()}`
-                },
-            })
-
-            const response = await fetch(url, {
-                method: "DELETE",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${getToken()}`
-                },
-            })
-
-            window.location.href = `/dashboard`;
-
-            if (!responseData.ok && !response.ok) {
-
-                console.error(`une erreur lors de la suppression de l'ESP`);
-                console.error(await responseData.json())
-                console.error(await response.json());
-
-            }
-        } catch (error) {
-            console.error("Error: ", error)
-        }
-
-    };
-
-    return (
-        <div className="flex cursor-pointer gap-2">
-            <Trash2
-                onClick={() => deleteEsp(id)}
-            />
-        </div>
-    )
+  return (
+    <div className="flex cursor-pointer gap-2">
+      <Trash2 onClick={() => deleteEsp(id)} />
+    </div>
+  );
 }
