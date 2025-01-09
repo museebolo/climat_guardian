de6dfd0f2a82d2b772dec5c1d18e0164c6eaaf11
ColinRgm <colin.regamey@icloud.com>
2025-01-09 10:49:00 +0100

    fix: fixing the function
    Closes: #218

--- database/migration/deploy/new_way_delete_esp_permission.sql
+++ database/migration/deploy/new_way_delete_esp_permission.sql
@@ -1,26 +1,31 @@
 -- Deploy climat-guardian:new_way_delete_esp_permission to pg
 
 -- Function to DELETE on data and esp tables
-CREATE OR REMPLACE FUNCTION api.delete_esp({ id }: { id: string }) RETURN VOID AS $$
 
 BEGIN;
 
 
 -- Remove permission to DELETE on data and esp tables
-REVOKE DELETE ON api.data TO web_user;
-REVOKE DELETE ON api.esp TO web_user;
-
+REVOKE DELETE ON api.data from web_user;
+REVOKE DELETE ON api.esp from web_user;
 
 -- Add permission to DELETE on data and esp tables
 GRANT DELETE ON api.data TO web_user;
 GRANT DELETE ON api.esp TO web_user;
 
+-- Delete the function if exists
+DROP FUNCTION IF EXISTS api.delete_data;
 
--- DELETE on data and esp tables
-DELETE FROM api.data WHERE id = ${id}
-DELETE FROM api.esp WHERE id = ${id}
+-- Create the function
+CREATE OR REPLACE FUNCTION api.delete_data(id)
+    RETURNS VOID AS $$
+
+BEGIN
+    -- DELETE on data and esp tables
+    DELETE FROM api.data WHERE id = id;
+    DELETE FROM api.esp WHERE id = id;
+END;
+$$ LANGUAGE plpgsql;
 
 
 END;
-
-$$ LANGUAGE plpgsql;
