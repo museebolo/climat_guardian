9921cca40ace58642df1a1ebc102e519bb7730e8
ColinRgm <colin.regamey@icloud.com>
2025-01-14 11:33:18 +0100

    fix: some corrections
    Closes: #218

--- database/migration/revert/new_way_delete_esp_permission.sql
+++ database/migration/revert/new_way_delete_esp_permission.sql
@@ -6,9 +6,5 @@ BEGIN;
 -- DELETE the function if it exists
 DROP FUNCTION IF EXISTS api.delete_esp_data_and_esp(id);
 
--- Remove permission to delete
-REVOKE DELETE ON api.esp FROM web_user; -- no one can delete ESP
-REVOKE DELETE ON api.data FROM web_user; -- no one can delete ESP's data
-
 
 COMMIT;
