c585a039f2d543f636e514d5e0d9e9199ec73472
ColinRgm <colin.regamey@icloud.com>
2025-01-21 15:30:22 +0100

    fix: reformat
    Closes: #230

--- nextjs-interface/src/app/ui/dashboard/DeleteUsersData.tsx
+++ nextjs-interface/src/app/ui/dashboard/DeleteUsersData.tsx
@@ -9,12 +9,7 @@ import { Trash2 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useAllUsers } from "@/lib/data";
 
-export default function DeleteUserData({
-  username
-}: {
-  username: string;
-}) {
-
+export default function DeleteUserData({ username }: { username: string }) {
   const [users, setUsers] = useState<user[]>([]);
   const allUsers = useAllUsers();
 
@@ -40,10 +35,9 @@ export default function DeleteUserData({
       });
 
       if (!response.ok) {
-
         return;
       } else {
-        window.location.href = '/dashboard/users'
+        window.location.href = "/dashboard/users";
       }
 
       // Remove user from local state after successful deletion
@@ -54,31 +48,29 @@ export default function DeleteUserData({
   };
 
   return (
-    <div>
-      <div className="flex cursor-pointer gap-2">
-        <Popover>
-          <PopoverTrigger>
-            <Trash2 />
-          </PopoverTrigger>
+    <div className="flex cursor-pointer gap-2">
+      <Popover>
+        <PopoverTrigger>
+          <Trash2 />
+        </PopoverTrigger>
 
-          <PopoverContent className="mr-5 mt-2 flex w-fit flex-col gap-2 dark:bg-zinc-800">
-            <p>Supprimer cet utilisateur ?</p>
-            <Button
-              onClick={async () => {
-                try {
-                  await handleDelete(username);
-                } catch (e) {
-                  console.error(e);
-                }
-              }}
-              className="w-72"
-            >
-              OUI
-            </Button>
-            <Button className="w-72">NON</Button>
-          </PopoverContent>
-        </Popover>
-      </div>
+        <PopoverContent className="mr-5 mt-2 flex w-fit flex-col gap-2 dark:bg-zinc-800">
+          <p>Supprimer cet utilisateur ?</p>
+          <Button
+            onClick={async () => {
+              try {
+                await handleDelete(username);
+              } catch (e) {
+                console.error(e);
+              }
+            }}
+            className="w-72"
+          >
+            OUI
+          </Button>
+          <Button className="w-72">NON</Button>
+        </PopoverContent>
+      </Popover>
     </div>
   );
 }
