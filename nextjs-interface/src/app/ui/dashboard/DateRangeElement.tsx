00df528b2cf015f846e2a86ae67f64f3f9604b69
Cédric Gaudin <cedric.gaudin@museebolo.ch>
2026-08-19 23:10:07 +0200

    chore: migrate react-day-picker to v10

--- nextjs-interface/src/app/ui/dashboard/DateRangeElement.tsx
+++ nextjs-interface/src/app/ui/dashboard/DateRangeElement.tsx
@@ -48,7 +48,7 @@ export function DateRangeElement({
         </PopoverTrigger>
         <PopoverContent className="w-auto p-0" align="start">
           <Calendar
-            initialFocus
+            autoFocus
             mode="range"
             defaultMonth={date?.from}
             selected={date}
